5ed19a88d7a6f84c38be8af54d6d3870c7cab5fe
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2022-12-28 21:56:51 +0000

    Initial commit.
    Fixed the mess ChatGPT suggested.  It had the right general idea though to be fair, and I've kept its code structure.

---
+++ bb26.py
@@ -0,0 +1,24 @@
+def BB26_to_int(s: str) -> int:
+
+    result = 0
+   
+    for c in s:
+        result *= 26
+        result += 1 + ord(c) - ord('a')
+    
+    return result
+    
+    
+def int_to_BB26(n: int) -> str:
+
+    result = ""
+
+    while n > 0:
+        n, remainder = divmod(n, 26)
+        
+        if remainder == 0:
+            remainder = 26
+            n -= 1
+        
+        result = chr(ord('a') + (remainder - 1)) + result
+    return result
