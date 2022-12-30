a4321cb266f4ea9b69d06af4850799b8b04093e4
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2022-12-30 11:57:58 +0000

    Prime factorisations in BB26.
    Started with ChatGPT factorisation function, and added cache of primes.

---
+++ prime_factors.py
@@ -0,0 +1,42 @@
+
+import collections
+from bb26 import BB26_to_int, int_to_BB26
+
+
+primes = {} # Must start empty.
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    
+    if primes and max(primes.keys())**2 < n:
+        known_primes = iter(())
+    else:
+        known_primes = iter(tuple(primes.keys()))
+    
+    while i * i <= n:
+        if n % i:
+            i = next(known_primes, i+1)
+        else:
+            n //= i
+            factors.append(i)
+            primes[i] = None
+    if n > 1:
+        factors.append(n)
+    return factors
+    
+    
+def prettify_factors(factors, convert = lambda x: x):
+    
+    if len(factors) <= 1:
+        return "neither prime nor composite" if factors == [] else "prime number"
+        
+    return '*'.join(f'{convert(factor)}^{convert(freq)}' if freq > 1 else f'{convert(factor)}' 
+                   for factor, freq in collections.Counter(factors).items()
+                  )
+                  
+                  
+print('BB26 prime factorisations: ')           
+for x in range(680, 703):
+    print(f'{int_to_BB26(x)}={prettify_factors(prime_factors(x), int_to_BB26)}')
